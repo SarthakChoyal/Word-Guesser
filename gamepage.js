8cba3dbb83c9ffc8fa7191524f647a05ddd9bb48
Sarthak Choyal <94727252+SarthakChoyal@users.noreply.github.com>
2023-02-04 10:15:51 +0000

    Add files via upload

--- gamepage.js
+++ gamepage.js
@@ -13,6 +13,7 @@ document.getElementById("player2_score").innerHTML = player2_score + " : ";
 document.getElementById("player_question").innerHTML = "Your turn to input a word 🟩" + player1_name;
 document.getElementById("player_answer").innerHTML = "Your turn to guess the word 🟥" + player2_name;
 
+word ="";
 
 function send() {
     get_word = document.getElementById("word").value;
@@ -41,4 +42,51 @@ function send() {
     row = question_word + input_box + check_button ; 
     document.getElementById("output").innerHTML = row;
 document.getElementById("word").value = "";
+}
+
+answer_turn = "player2";
+question_turn = "player1";
+
+
+function check() {
+    get_answer = document.getElementById("input_check_box").value;
+    answer = get_answer.toLowerCase();
+    console.log("Answer in lower case - " + answer);
+    if(answer == word)
+    {
+        if(answer_turn == "player1")
+        {
+            player1_score = player1_score +1;
+            document.getElementById("player1_score").innerHTML = player1_score;
+        }
+        else
+        {
+            player2_score = player2_score +1;
+            document.getElementById("player2_score").innerHTML = player2_score;
+        }
+    }
+    if(question_turn == "player1")
+    {
+        console.log("Switching question turn from player 1 to player 2");
+        question_turn = "player2"
+        document.getElementById("player_question").innerHTML = "Question Turn - " + player2_name;
+    }
+    else
+    {
+        console.log("Switching question turn from player 2 to player 1")
+        question_turn = "player1"
+        document.getElementById("player_question").innerHTML = "Question Turn - " + player1_name;
+    }
+
+    if(answer_turn == "player1")
+    {
+        answer_turn = "player2"
+        document.getElementById("player_answer").innerHTML = "Answer Turn - " + player2_name;
+    }
+    else
+    {
+        answer_turn = "player1"
+        document.getElementById("player_answer").innerHTML = "Answer Turn -" + player1_name;
+    }
+    document.getElementById("output").innerHTML = "";
 }
